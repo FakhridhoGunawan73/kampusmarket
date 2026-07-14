a9ca8f86b2d8d404c52479adcbe242a1bac51b3f
FakhridhoGunawan73 <fakhridhogunawan01@gmail.com>
2026-07-14 17:20:47 +0700

    Create login screen UI

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,15 +1,72 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, TextInput, Button } from "react-native-paper";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <Text variant="headlineMedium" style={styles.title}>
+        KampusMarket
+      </Text>
+
+      <Text variant="bodyMedium" style={styles.subtitle}>
+        Marketplace Barang Bekas Mahasiswa
+      </Text>
+
+      <TextInput
+        label="Email"
+        mode="outlined"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Password"
+        mode="outlined"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+      />
+
+      <Button
+        mode="contained"
+        style={styles.button}
+      >
+        Login
+      </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+
+  title: {
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  subtitle: {
+    textAlign: "center",
+    marginBottom: 30,
+  },
+
+  input: {
+    marginBottom: 15,
+  },
+
+  button: {
+    marginTop: 10,
+    paddingVertical: 5,
+  },
+});
